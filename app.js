6502bc22d98e50d887489fc645b60210b2ea8c44
Yellow10041 <40462438+Yellow10041@users.noreply.github.com>
2022-03-17 18:52:10 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+$(function() {
+
+    $('.button__inner').click(function() {
+        $('.device__item__price').css({'opacity' : '0.7'});
+        $('.device__item__cross').css({'width' : '60px'});
+        $('.device__item__sale').css({'width' : '60px', 'margin-left' : '15px'});
+        $('.device__item__price').each(function(index) {
+            $price = $(this).children('span').text() * 0.9;
+            console.log($price, $price.toFixed());
+            $('.device__item__sale').eq(index).children('span').text($price.toFixed());
+        });
+    });
+
+});

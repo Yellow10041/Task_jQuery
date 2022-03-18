ece5934a052ddeda3dffe2321e121261a1d53c65
Yellow10041 <40462438+Yellow10041@users.noreply.github.com>
2022-03-18 12:32:52 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ $(function() {
         $('.device__item__cross').css({'width' : '60px'});
         $('.device__item__sale').css({'width' : '60px', 'margin-left' : '15px'});
         $('.device__item__price').each(function(index) {
-            $price = $(this).children('span').text() * 0.9;
-            console.log($price, $price.toFixed());
-            $('.device__item__sale').eq(index).children('span').text($price.toFixed());
+            let price = $(this).children('span').text() * 0.9;
+            console.log(price, price.toFixed());
+            $('.device__item__sale').eq(index).children('span').text(price.toFixed());
         });
     });
 
